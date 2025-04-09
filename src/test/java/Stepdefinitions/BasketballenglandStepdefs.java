dbd1be22547f094fa60566e23dd4746a863cd1eb
Fredrik Högbom <Fhogbom@gmail.com>
2025-04-09 11:30:11 +0200

    Skrivit tester för -Felmeddelande om man inte checkat i terms and conditions
    Samt kontroller för
    -Att vi når konfirmationssidan

--- src/test/java/Stepdefinitions/BasketballenglandStepdefs.java
+++ src/test/java/Stepdefinitions/BasketballenglandStepdefs.java
@@ -6,7 +6,6 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
